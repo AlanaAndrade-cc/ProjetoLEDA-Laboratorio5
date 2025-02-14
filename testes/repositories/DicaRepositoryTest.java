188627a6161c9ee38af0c52ed538e5f0d4c39a8b
Alana Andrade <alana.vanessa.pimentel.toldo.andrade@ccc.ufcg.edu.br>
2025-02-14 12:08:09 -0300

    testes implementados

--- testes/repositories/DicaRepositoryTest.java
+++ testes/repositories/DicaRepositoryTest.java
@@ -1,5 +1,115 @@
 package repositories;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import entities.Dica;
+import entities.Usuario;
+
 public class DicaRepositoryTest {
+
+	private DicaRepository repository;
+	private Usuario usuarioA;
+	private Usuario usuarioB;
+	private Dica dica1;
+	private Dica dica2;
+	private Dica dica3;
+	private Dica dica4;
+
+	@BeforeEach
+	void setUp() {
+		this.repository = new DicaRepository();
+		this.usuarioA = new Usuario("Alana Vanessa", "123.456.789-00", "147258", "123210882");
+		this.usuarioB = new Usuario("Camila Rodrigues", "234.567.891-00", "258369", "123210240");
+		this.dica1 = new Dica(usuarioA, "Monitoria");
+		this.dica2 = new Dica(usuarioA, "PesquisaExtensao");
+		this.dica3 = new Dica(usuarioB, "Estagio");
+	}
+
+	@Test
+	void testAdicionaDicaSucesso() {
+		assertEquals(1, repository.adicionaDica(dica1));
+		assertEquals(2, repository.adicionaDica(dica2));
+		assertEquals(3, repository.adicionaDica(dica3));
+	}
+
+	@Test
+	void testAdicionaDicaNula() {
+		String mensagem = assertThrows(NullPointerException.class, () -> {
+			repository.adicionaDica(dica4);
+		}).getMessage();
+		assertEquals("A dica não pode ser nula", mensagem);
+	}
+
+	@Test
+	void testListaDicasListaVazia() {
+		assertArrayEquals(new String[0], repository.listaDicas());
+	}
+
+	@Test
+	void testListaDicasComDicas() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		String[] lista = repository.listaDicas();
+		assertEquals("Autor: Alana Vanessa, tema: Monitoria", lista[0]);
+		assertEquals("Autor: Camila Rodrigues, tema: Estagio", lista[1]);
+	}
+
+	@Test
+	void testListaDicasDetalhes() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		dica1.adicionaTexto(usuarioA, "Prepare materiais antes dos atendimentos.");
+
+		String[] detalhes = repository.listaDicasDetalhes();
+		assertEquals("Autor: Alana Vanessa\nPrepare materiais antes dos atendimentos. (41 caracteres)", detalhes[0]);
+		assertEquals("Autor: Camila Rodrigues", detalhes[1]);
+	}
+
+	@Test
+	void testListaDica() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		assertEquals("Autor: Camila Rodrigues, tema: Estagio", repository.listaDica(2));
+	}
+
+	@Test
+	void testListaDicaDetalhes() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		dica1.adicionaTexto(usuarioA, "Prepare materiais antes dos atendimentos.");
+
+		assertEquals("Autor: Alana Vanessa\nPrepare materiais antes dos atendimentos. (41 caracteres)",
+				repository.listaDicaDetalhes(1));
+	}
+
+	@Test
+	void testBuscaDicaSucesso() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		Dica encontrada = repository.buscaDica(1);
+
+		assertEquals(dica1, encontrada);
+	}
+
+	@Test
+	void testBuscaDicaPosicaoInvalida() {
+		repository.adicionaDica(dica1);
+		repository.adicionaDica(dica3);
+
+		String mensagem = assertThrows(IllegalArgumentException.class, () -> {
+			repository.buscaDica(3);
+		}).getMessage();
+		assertEquals("A posição da dica é inválida", mensagem);
+	}
 
 }
--- testes/repositories/UsuarioRepositoryTest.java
+++ testes/repositories/UsuarioRepositoryTest.java
@@ -1,5 +1,101 @@
 package repositories;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import entities.Usuario;
+
 public class UsuarioRepositoryTest {
+
+	private UsuarioRepository repository;
+	private Usuario usuarioA;
+	private Usuario usuarioB;
+	private Usuario usuarioC;
+	private Usuario usuarioD;
+	private Usuario usuarioE;
+
+	@BeforeEach
+	void setUp() {
+		this.repository = new UsuarioRepository();
+		this.usuarioA = new Usuario("Alana Vanessa", "123.456.789-00", "147258", "123210882");
+		this.usuarioB = new Usuario("Camila Rodrigues", "234.567.891-00", "258369", "123210240");
+		this.usuarioC = new Usuario("Joao Victor", "345.678.912-00", "369147", "123210456");
+		this.usuarioD = new Usuario("Lorena Nascimento", "456.789.123-00", "159753", "123211071");
+		this.usuarioE = new Usuario("Lukas Soares", "567.891.234-00", "357951", "123210298");
+	}
+
+	@Test
+	void testAdicionaEstudanteSucesso() {
+		assertTrue(repository.adicionaEstudante(usuarioA.getCpf(), usuarioA));
+		assertTrue(repository.adicionaEstudante(usuarioB.getCpf(), usuarioB));
+	}
+
+	@Test
+	void testAdicionaEstudanteDuplicado() {
+		repository.adicionaEstudante(usuarioC.getCpf(), usuarioC);
+		assertFalse(repository.adicionaEstudante(usuarioC.getCpf(), usuarioC));
+	}
+
+	@Test
+	void testListaEstudantesOrdemAlfabetica() {
+		repository.adicionaEstudante(usuarioE.getCpf(), usuarioE);
+		repository.adicionaEstudante(usuarioC.getCpf(), usuarioC);
+		repository.adicionaEstudante(usuarioA.getCpf(), usuarioA);
+
+		String[] estudantes = repository.listaEstudantes();
+		assertEquals(usuarioA.toString(), estudantes[0]);
+		assertEquals(usuarioC.toString(), estudantes[1]);
+		assertEquals(usuarioE.toString(), estudantes[2]);
+	}
+
+	@Test
+	void testListaEstudantesRankingDicas() {
+		repository.adicionaEstudante(usuarioA.getCpf(), usuarioA);
+		repository.adicionaEstudante(usuarioD.getCpf(), usuarioD);
+		repository.adicionaEstudante(usuarioB.getCpf(), usuarioB);
+
+		usuarioD.recebeBonificacao(70);
+		usuarioB.recebeBonificacao(20);
+
+		String[] estudantes = repository.listaEstudantesRankingDicas();
+		assertEquals(usuarioD.toString(), estudantes[0]);
+		assertEquals(usuarioB.toString(), estudantes[1]);
+		assertEquals(usuarioA.toString(), estudantes[2]);
+	}
+
+	@Test
+	void testBuscaEstudanteSucesso() {
+		repository.adicionaEstudante(usuarioA.getCpf(), usuarioA);
+		Usuario encontrado = repository.buscaEstudante("123.456.789-00", "147258");
+
+		assertNotNull(encontrado);
+		assertEquals(encontrado, usuarioA);
+	}
+
+	@Test
+	void testBuscaEstudanteSenhaIncorreta() {
+		repository.adicionaEstudante(usuarioA.getCpf(), usuarioA);
+
+		String mensagem = assertThrows(IllegalArgumentException.class, () -> {
+			repository.buscaEstudante("123.456.789-00", "147853");
+		}).getMessage();
+		assertEquals("Usuário ou senha inválidos", mensagem);
+	}
+
+	@Test
+	void testBuscaEstudanteNaoCadastrado() {
+		repository.adicionaEstudante(usuarioA.getCpf(), usuarioA);
+
+		String mensagem = assertThrows(NullPointerException.class, () -> {
+			repository.buscaEstudante("456.789.123-00", "159753");
+		}).getMessage();
+		assertEquals("O usuário não pode ser nulo", mensagem);
+	}
 
 }
